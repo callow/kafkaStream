90295a1d6ab4b2aefe0aa89625f67e264331df1b
Song Lei <Lei.Song@duotechglobal.com>
2022-10-31 19:57:28 +0800

    update

---
+++ src/main/java/kafka/stream/KafkaHelper.java
@@ -0,0 +1,46 @@
+package kafka.stream;
+
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsConfig;
+
+public class KafkaHelper {
+	
+	 public final static String BOOTSTRAP_SERVER = "localhost:9092";
+	 
+	 public static final String FIRST_APP_ID = "first_streams_app_id";
+	 public static final String FIRST_APP_SOURCE_TOPIC = "input.words";
+	 public static final String FIRST_APP_TARGET_TOPIC = "output.words";
+	 
+	 
+	 public static void info(String msg) {
+		 System.out.println(msg);
+	 }
+	 
+	 public static Properties config(String appId) {
+        final Properties properties = new Properties();
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG, appId);
+        return properties;
+	 }
+	 
+	 public static void start(KafkaStreams kafkaStreams) {
+	       CountDownLatch latch = new CountDownLatch(1);
+	        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+	        	//6. stop(graceful)
+	            kafkaStreams.close();
+	            latch.countDown();
+	        }));
+
+	        //5. start
+	        kafkaStreams.start();
+	        System.out.println("Kafka stream done!");
+	        try {
+				latch.await();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+	 }
+}
---
+++ src/main/java/kafka/stream/first/First.java
@@ -0,0 +1,43 @@
+package kafka.stream.first;
+
+import java.util.Properties;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Named;
+import org.apache.kafka.streams.kstream.Produced;
+
+import kafka.stream.KafkaHelper;
+public class First {
+	
+    public static void main(String[] args) throws InterruptedException {
+        
+    	// 1. create configuration
+        Properties properties = KafkaHelper.config(KafkaHelper.FIRST_APP_ID);
+        
+        // 2. create StreamsBuilder 
+        StreamsBuilder builder = new StreamsBuilder();
+        builder.stream(KafkaHelper.FIRST_APP_SOURCE_TOPIC, Consumed.with(Serdes.String(), Serdes.String()).withName("source-processor")
+                        .withOffsetResetPolicy(Topology.AutoOffsetReset.LATEST))
+                
+        		.peek((k, v) -> KafkaHelper.info("[source] value:  " + v), Named.as("pre-transform-peek"))
+                
+                .filter((k, v) -> v != null && v.length() > 5, Named.as("filter-processor"))
+                
+                .mapValues(v -> v.toUpperCase(), Named.as("map-processor"))
+                
+                .peek((k, v) -> KafkaHelper.info("[target] value: " + v), Named.as("post-transform-peek"))
+                
+                .to(KafkaHelper.FIRST_APP_TARGET_TOPIC, Produced.with(Serdes.String(), Serdes.String()).withName("sink-processor"));
+        
+        // 3. create topology
+        Topology topology = builder.build();
+        
+        // 4. start stream
+        KafkaHelper.start(new KafkaStreams(topology, properties));
+        
+    }
+}
